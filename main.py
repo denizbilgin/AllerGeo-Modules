759c07ea60bab34ef9237864d199614d715069e5
Deniz Bilgin <denizbilgin156@gmail.com>
2024-11-28 18:47:33 +0300

    28.11.2024 data fetched

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from Utils import *
 import pandas as pd
 from VegetationCollector import *
 from UnicodeTR import UnicodeTR
+from git import Repo
+import os
+from datetime import datetime
 
 
 if __name__ == '__main__':
-    pollen_data_collector = AccuWeather()
-    vegetation_collector = PlantNet()
-    pollen_data_collector.save_aegean("aegean_weather_data.csv")
+    #pollen_data_collector = AccuWeather()
+    #vegetation_collector = PlantNet()
+    #pollen_data_collector.save_aegean("aegean_weather_data.csv")
 
     #district_name = "Ula"
     #district_name = UnicodeTR(district_name).capitalize()
@@ -19,4 +22,15 @@ if __name__ == '__main__':
     #    print(individual_species)
     #    print()
 
+    repo = Repo(os.getcwd())
+    index = repo.index
+    index.add("*")
 
+    current_date = datetime.now().strftime("%d.%m.%Y")
+    commit_message = current_date + " data fetched"
+
+    index.commit(commit_message)
+
+    origin = repo.remote(name="origin")
+    origin.push()
+    print("All files added, committed, and pushed successfully!")
