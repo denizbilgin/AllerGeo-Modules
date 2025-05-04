1c7e3a39e37876b8dce97e15fbdeebb05fbd26a3
Deniz Bilgin <denizbilgin156@gmail.com>
2025-05-04 13:39:54 +0300

    04.05.2025 data fetched

--- WeatherDataCollector.py
+++ WeatherDataCollector.py
@@ -6,7 +6,7 @@ import requests
 from Utils import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
